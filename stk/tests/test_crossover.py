7780e6b3df1d13a9b8d381a0cfe5650662c5fa2c
Lukas Turcani <lukasturcani93@gmail.com>
2018-05-11 11:35:24 +0100

    fixed test

--- stk/tests/test_crossover.py
+++ stk/tests/test_crossover.py
@@ -1,6 +1,8 @@
 from ..ga import Crossover, Population
-from ..molecular import Molecule
+from ..molecular import Molecule, CACHE_SETTINGS
 from os.path import join
+
+CACHE_SETTINGS['ON'] = True
 
 pop = Population.load(join('data', 'crossover', 'molecules.json'),
                       Molecule.from_dict)
@@ -11,11 +13,13 @@ def test_bb_lk_exchange():
     parent_tops = {m1.topology.__class__, m2.topology.__class__}
     offspring_pop = Crossover.bb_lk_exchange(None, m1, m2)
     for offspring in offspring_pop:
+
         assert offspring.topology.__class__ in parent_tops
         bb1, bb2 = offspring.building_blocks
+
         if bb1 in m1.building_blocks:
             assert bb2 not in m1.building_blocks
             assert bb2 in m2.building_blocks
         else:
             assert bb2 in m1.building_blocks
-            assert bb1 not in m2.building_blocks
+            assert bb2 not in m2.building_blocks
